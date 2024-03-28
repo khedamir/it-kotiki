f755ba7029d19c4692187d080be89dee316c7ccd
Kirill Konovalov <auddax@gmail.com>
2024-03-28 19:13:33 +0300

    IK-25 feat: add Modal component

---
+++ packages/client/src/components/Modal/constants/ModalConfig.tsx
@@ -0,0 +1,10 @@
+import { EMODAL_TYPE, IModalConfig } from '../models/models';
+
+export const MODAL_CONFIG: Record<EMODAL_TYPE, IModalConfig> = {
+	[EMODAL_TYPE.GAME_END]: {
+		title: 'Игра завершена',
+		content: 'Спасибо, что приняли участие в нашей игре!\nЖдем вас снова',
+		cancelText: 'Вернуться на главную',
+		okText: 'Начать сначала?',
+	},
+};
