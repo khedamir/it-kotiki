22b6861bb1c709b213ffd0ff85538a1e460d7681
Kirill Konovalov <auddax@gmail.com>
2024-03-30 10:18:15 +0300

    IK-35 refactor: move mock test data to separate modules

--- packages/client/src/utils/helpers.test.ts
+++ packages/client/src/utils/helpers.test.ts
@@ -1,99 +1,24 @@
 import { objectSorter } from './helpers';
+import { mockLeaderboardItems, expectedNoOrder, expectedWithOrder } from './helpers.mock';
 
 describe('Testing helpers functions', () => {
 	describe('Testing objectsSorter function', () => {
-		const data = [
-			{
-				avatarPath: '',
-				id: 1,
-				playerName: 'Игрок 1',
-				scoreToday: 18,
-				scoreTotal: 143,
-				userPosition: 1,
-			},
-			{
-				avatarPath: '',
-				id: 2,
-				playerName: 'Игрок 2',
-				scoreToday: 4,
-				scoreTotal: 114,
-				userPosition: 2,
-			},
-			{
-				avatarPath: '',
-				id: 3,
-				playerName: 'Игрок 3',
-				scoreToday: 15,
-				scoreTotal: 165,
-				userPosition: 3,
-			},
-		];
 		it('returns passed array if no sorting key provided', () => {
-			const result = objectSorter(data);
-			expect(JSON.stringify(result)).toBe(JSON.stringify(data));
+			const result = objectSorter(mockLeaderboardItems);
+			expect(JSON.stringify(result)).toBe(JSON.stringify(mockLeaderboardItems));
 		});
 		it('returns array, sorted by key in ascending order if order parameter is not provided', () => {
 			const key = 'scoreToday';
-			const expected = [
-				{
-					avatarPath: '',
-					id: 2,
-					playerName: 'Игрок 2',
-					scoreToday: 4,
-					scoreTotal: 114,
-					userPosition: 2,
-				},
-				{
-					avatarPath: '',
-					id: 3,
-					playerName: 'Игрок 3',
-					scoreToday: 15,
-					scoreTotal: 165,
-					userPosition: 3,
-				},
-				{
-					avatarPath: '',
-					id: 1,
-					playerName: 'Игрок 1',
-					scoreToday: 18,
-					scoreTotal: 143,
-					userPosition: 1,
-				},
-			];
-			const result = objectSorter(data, key);
-			expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+
+			const result = objectSorter(mockLeaderboardItems, key);
+			expect(JSON.stringify(result)).toBe(JSON.stringify(expectedNoOrder));
 		});
 		it('returns array, sorted by key in descending order if order parameter is provided', () => {
 			const key = 'scoreToday';
 			const descOrder = true;
-			const expected = [
-				{
-					avatarPath: '',
-					id: 1,
-					playerName: 'Игрок 1',
-					scoreToday: 18,
-					scoreTotal: 143,
-					userPosition: 1,
-				},
-				{
-					avatarPath: '',
-					id: 3,
-					playerName: 'Игрок 3',
-					scoreToday: 15,
-					scoreTotal: 165,
-					userPosition: 3,
-				},
-				{
-					avatarPath: '',
-					id: 2,
-					playerName: 'Игрок 2',
-					scoreToday: 4,
-					scoreTotal: 114,
-					userPosition: 2,
-				},
-			];
-			const result = objectSorter(data, key, descOrder);
-			expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+
+			const result = objectSorter(mockLeaderboardItems, key, descOrder);
+			expect(JSON.stringify(result)).toBe(JSON.stringify(expectedWithOrder));
 		});
 	});
 });
